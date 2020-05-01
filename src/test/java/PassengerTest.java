5fde8acd6fbf15053fbd8c771b35429232e4ecd8
Ariane Holford <aribomop@gmail.com>
2020-05-01 14:45:20 +0100

    first commit

---
+++ src/test/java/PassengerTest.java
@@ -0,0 +1,20 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class PassengerTest {
+
+    Passenger passenger1;
+
+    @Before
+    public void before(){
+        passenger1 = new Passenger("Sandara", 2);
+    }
+
+    @Test
+    public void passengerHasAName(){
+        assertEquals("Sandara", passenger1.getName());
+    }
+
+}
